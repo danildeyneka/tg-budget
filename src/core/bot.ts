fd6ca1ee66542713e004080e2447c55ed2ed3971
Daniel <danildeyneka@gmail.com>
2025-10-24 22:17:11 +0500

    chore: add dev environment

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -4,18 +4,21 @@ import { Bot } from 'grammy'
 import { botHandlers } from '../handlers/index.ts'
 import { handleError } from '../helpers/errors.ts'
 import { loadHandlers } from '../helpers/load-handlers.ts'
+import { initHealthServer } from '../services/server/health-server.ts'
 import type { MyContext } from '../types/context.ts'
 
-import '../services/server/health-server.ts'
-
 const bootstrap = async () => {
-  const bot = new Bot<MyContext>(process.env.BOT_API_TOKEN!)
+  const isDev = !!process.env.DEV_MODE
+
+  const bot = new Bot<MyContext>(isDev ? process.env.BOT_API_TOKEN_DEV! : process.env.BOT_API_TOKEN!)
 
   loadHandlers(
     bot,
     botHandlers,
   )
   bot.catch(err => handleError(err))
+
+  initHealthServer()
 
   await bot.start()
 }
--- src/services/server/health-server.ts
+++ src/services/server/health-server.ts
@@ -2,11 +2,17 @@ import { createServer } from 'node:http'
 
 const PORT = process.env.PORT || 3000
 
-const healthServer = createServer((_, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Server is running')
-})
+export const initHealthServer = () => {
+  const isDev = !!process.env.DEV_MODE
 
-healthServer.listen(PORT, () => {
-  console.log('Server is listening on port ' + PORT)
-})
+  if (isDev) return console.log('DEVELOPMENT MODE')
+
+  const healthServer = createServer((_, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('Server is running')
+  })
+
+  healthServer.listen(PORT, () => {
+    console.log('Server is listening on port ' + PORT)
+  })
+}
