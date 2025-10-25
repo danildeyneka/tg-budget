0f501c2aa3dd783db906436f466cf90ed7550fb4
Daniel <danildeyneka@gmail.com>
2025-10-25 22:27:38 +0500

    chore: add keep-alive for hosting

--- src/services/server/health-server.ts
+++ src/services/server/health-server.ts
@@ -1,9 +1,11 @@
 import { createServer } from 'node:http'
 
 const PORT = process.env.PORT || 3000
+const NINE_MINUTES = 9 * 60 * 10_000
 
 export const initHealthServer = () => {
   const isDev = !!process.env.DEV_MODE
+  const serverUrl = process.env.PING_HOSTING_URL
 
   if (isDev) return console.log('DEVELOPMENT MODE')
 
@@ -15,4 +17,17 @@ export const initHealthServer = () => {
   healthServer.listen(PORT, () => {
     console.log('Server is listening on port ' + PORT)
   })
+
+  if (!serverUrl && !isDev) return
+  setInterval(() => ping(serverUrl!), NINE_MINUTES)
+}
+
+async function ping(server: string) {
+  try {
+    const res = await fetch(server).then(res => res.ok)
+    if (res) console.log(`Pinged successfully at ${new Date().toLocaleString()}`)
+  }
+  catch (err) {
+    console.log('Error in ping', err)
+  }
 }
