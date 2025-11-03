294027c7fa6cb7a98581c790148d75f4771507d3
Daniel <danildeyneka@gmail.com>
2025-11-03 17:12:56 +0500

    refactor: rename access composer handler

--- src/core/middleware/access.ts
+++ src/core/middleware/access.ts
@@ -1,9 +1,9 @@
 import { Composer } from 'grammy'
 import type { MyContext } from '../../types/context.ts'
 
-export const allowedAccess = new Composer<MyContext>()
+export const accessComposer = new Composer<MyContext>()
 
-allowedAccess.use(async (ctx, next) => {
+accessComposer.use(async (ctx, next) => {
   const allowedUsers = (process.env.ALLOWED_USERS || '').split(',')
   if (allowedUsers.includes(ctx.from?.id + '')) {
     await next()
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,5 +1,5 @@
-import { type Composer } from 'grammy'
-import { allowedAccess } from '../core/middleware/access.ts'
+import { Composer } from 'grammy'
+import { accessComposer } from '../core/middleware/access.ts'
 import { sessionComposer } from '../core/middleware/session.ts'
 import { databaseComposer } from '../services/database/connector.ts'
 import type { MyContext } from '../types/context.ts'
@@ -7,8 +7,8 @@ import { routerComposer } from './callbacks/index.ts'
 import { expensesComposer } from './commands/expenses/index.ts'
 import { startComposer } from './commands/start.ts'
 
-export const botHandlers: Array<Composer<MyContext>> = [
-  allowedAccess,
+export const botComposers: Array<Composer<MyContext>> = [
+  accessComposer,
   databaseComposer,
   sessionComposer,
   routerComposer,
